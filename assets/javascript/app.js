3818789c90a7cd88fdb87c226baa2fd4f6845a83
Patrick Knight <ptknight00@gmail.com>
2017-12-17 08:26:47 -0600

    js skeleton/variable init

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+//global var
+var questions;
+var game;
+
+function game init() {
+    questions = start();
+    game = resetGame();
+}
+//question and answers init/declare
+
+//render start screen
+
+//render question 
+    //randomly render answers
+
+//checking correct
+
+//onclicks
+    //start
+        //timer
+    //incorrect
+    //correct
+    //reset
